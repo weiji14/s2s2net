11a3d2886669fb08218c51acbed05d2e1ac83fc3
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-02-24 23:42:21 -0500

    :construction_worker: Setup Continuous Integration tests for S2S2Net
    Make sure that the neural network code runs! Using GitHub Actions CI to install the dependencies, do some linting, and test using pytest. The test is an integration test that feeds random tensor data into the neural network model, and we use Pytorch Lightning's fast_dev_run which makes it go through all the train/test/val/predict loops. Will make sure to expand this test suite in the future.

---
+++ tests/test_model.py
@@ -0,0 +1,38 @@
+"""
+Tests for S2S2Net.
+
+Based loosely on Pytorch Lightning's testing method described at
+https://github.com/PyTorchLightning/pytorch-lightning/blob/1.5.10/.github/CONTRIBUTING.md#how-to-add-new-tests
+"""
+import pytorch_lightning as pl
+import s2s2net.model
+import torch
+
+
+class RandomDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        return 2
+
+    def __getitem__(self, idx: int) -> dict:
+        return {"image": torch.randn(4, 512, 512), "mask": torch.randn(1, 2560, 2560)}
+
+
+def test_s2s2net():
+    """
+    Run a full train, val, test and prediction loop using 1 batch.
+    """
+    # Get some random data
+    dataloader = torch.utils.data.DataLoader(dataset=RandomDataset())
+
+    # Initialize Model
+    model: pl.LightningModule = s2s2net.model.S2S2Net()
+
+    # Training
+    trainer: pl.Trainer = pl.Trainer(fast_dev_run=True)
+    trainer.fit(model=model, train_dataloaders=dataloader)
+
+    # Test
+    # TODO
